5e64eb650b717e628137b57f70b71cd7bb951dfc
FentPams <feng.xinyi@northeastern.edu>
2022-06-22 01:27:48 -0700

    modify files

--- LAB07/src/Triangle.java
+++ LAB07/src/Triangle.java
@@ -2,60 +2,61 @@ import static java.lang.Math.sqrt;
 
 public class Triangle extends AbstractShape {
 
-  private Point2D point1;
-  private Point2D point2;
+  private final Point2D point2;
+  private final Point2D point3;
 
   /**
    * Constructs a triangle object with the given location of its reference point and other two
    * points
    *
-   * @param x  x coordinate of the reference point of this triangle
-   * @param y  y coordinate of the reference point of this triangle
-   * @param p1 The second point
-   * @param p2 The third point
+   * @param p1 The first point as reference
+   * @param p2 The second point
+   * @param p3 The third point
+   * @throw IllegalArgumentException
    */
-  public Triangle(double x, double y, Point2D p1, Point2D p2) throws IllegalArgumentException {
-    super(new Point2D(x, y));
-    if (!checkPoints(p1, p2)) {
-      this.point1 = p1;
+  public Triangle(Point2D p1, Point2D p2, Point2D p3) throws IllegalArgumentException {
+    super(p1);
+    // if three points are unique, triangle can be constructed
+    if (!checkThreePoints(p1, p2, p3)) {
       this.point2 = p2;
+      this.point3 = p3;
     } else {
-      throw new IllegalArgumentException("The two points are not unique");
+      throw new IllegalArgumentException("The points are not unique");
     }
   }
 
-  public Triangle(double x, double y, double x1, double y1, double x2, double y2) {
-    super(new Point2D(x, y));
-    if (!checkThreePoints(x, y, x1, y1, x2, y2)) {
-      this.point1 = new Point2D(x1, y1);
-      this.point2 = new Point2D(x2, y2);
-    } else {
-      throw new IllegalArgumentException("The two points are not unique");
-    }
+  /**
+   *  second constructor call the first constructor
+   * @param x1,x2,y1,y2,x3,y3, 6 double, each two will construct a point
+   */
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+      throws IllegalArgumentException {
+    this(new Point2D(x1, y1), new Point2D(x2, y2), new Point2D(x3, y3));
   }
 
 
   /**
    * Computes and returns the area of this shape.
-   *
+   * Area of triangle =  (Ax(By -Cy) + Bx(Cy -Ay) + Cx(Ay - By))/2
+   * Read more: https://www.java67.com/2016/10/how-to-calculate-area-of-triangle-in.html#ixzz7Wv6Dy1Gl
    * @return the area of the shape
    */
   @Override
   public double area() {
-    return (this.reference.getX() * (this.point1.getY() - this.point2.getY()) + this.point1.getX() *
-        (this.point2.getY() - this.reference.getY()) + this.point2.getX() *
-        (this.reference.getY() - this.point1.getY())) / 2;
+    return (this.reference.getX() * (this.point2.getY() - this.point3.getY()) + this.point2.getX() *
+        (this.point3.getY() - this.reference.getY()) + this.point3.getX() *
+        (this.reference.getY() - this.point2.getY())) / 2;
   }
 
   /**
    * Computes and returns the perimeter of this shape.
-   *
+   * invoke private helper method by adding three sides
    * @return the perimeter of the shape
    */
   @Override
   public double perimeter() {
-    return calculateDistance(this.reference, point1) + calculateDistance(this.reference, point2)
-        + calculateDistance(point2, point1);
+    return calculateDistance(this.reference, point2) + calculateDistance(this.reference, point3)
+        + calculateDistance(point2, point3);
   }
 
   /**
@@ -66,28 +67,59 @@ public class Triangle extends AbstractShape {
    */
   @Override
   public Shape resize(double factor) {
-    return null;
+    double lambda = sqrt(factor);
+    double x1 = this.reference.getX();
+    double y1 = this.reference.getY();
+    double x2 = this.point2.getX();
+    double y2 = this.point2.getY();
+    double x3 = this.point3.getX();
+    double y3 = this.point3.getY();
+    Point2D newPoint2 = new Point2D(x1 + lambda * (x2 - x1), y1 + lambda*(y2 - y1));
+    Point2D newPoint3 = new Point2D(x1 + lambda * (x3 - x1), y1 + lambda*(y3 - y1));
+    return new Triangle(this.reference, newPoint2, newPoint3);
   }
 
+  /**
+   *  check if two points are unique
+   * @param a point2d
+   * @param b point2d
+   * @return true if two points are not unique
+   */
   private boolean checkPoints(Point2D a, Point2D b) {
-    return a.getX() == b.getX() && a.getY() == b.getY() && a.distToOrigin() == b.distToOrigin();
+    return a.getX() == b.getX() && a.getY() == b.getY();
   }
 
-  private boolean checkThreePoints(double x, double y, double x1, double y1, double x2, double y2) {
-    return (Double.compare(x, x1) == 0 && Double.compare(x, x2) == 0 && Double.compare(x1, x2) == 0
-        && Double.compare(y, y1) == 0 && Double.compare(y, y2) == 0 && Double.compare(y1, y2) == 0);
+  /**
+   *  checks if three points are unique based on check points method
+   * @param a point2d
+   * @param b point2d
+   * @param c point2d
+   * @return boolean, if three points are not unique, return True
+   */
+  private boolean checkThreePoints(Point2D a, Point2D b, Point2D c) {
+    return checkPoints(a, b) && checkPoints(a, c) && checkPoints(b, c);
   }
 
+  /**
+   *  calculates the side of triangle
+   * @param x point2d
+   * @param y point2d
+   * @return double, the side of triangle
+   */
   private double calculateDistance(Point2D x, Point2D y) {
     return sqrt(
-        (x.getY() - y.getY()) * x.getY() - y.getY() + x.getX() - y.getX() * x.getX() - y.getX());
+        (x.getY() - y.getY()) * (x.getY() - y.getY()) + (x.getX() - y.getX()) * (x.getX() - y.getX()));
   }
 
+  /**
+   * to string method to display three points of the triangle
+   * @return string
+   */
   public String toString() {
     return String.format(
         "Triangle: Reference point:(%.3f,%.3f) Second point:(%.3f,%.3f) Third point:(%.3f,%.3f)",
-        this.reference.getX(), this.reference.getY(), this.point1.getX(), this
-            .point1.getY(), this.point2.getX(), this.point2.getY());
+        this.reference.getX(), this.reference.getY(), this.point2.getX(), this
+            .point2.getY(), this.point3.getX(), this.point3.getY());
   }
 
 }
--- LAB07/tests/ShapeTest.java
+++ LAB07/tests/ShapeTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class ShapeTest {
 
   Shape circle1, circle2, circle3, rect1, rect2, tri1, tri2, tri3;
-  Point2D point1, point2;
+  Point2D point1, point2, point3;
 
   @Before
   public void setup() {
@@ -20,13 +20,13 @@ public class ShapeTest {
     rect1 = new Rectangle(5, 6, 2.5, 2);
     rect2 = new Rectangle(2, 3, 10, 10);
 
-    point1 = new Point2D(4, 5);
-    point2 = new Point2D(6, 7);
+    point1 = new Point2D(0, 0);
+    point2 = new Point2D(3, 0);
+    point3 = new Point2D(0,4);
     tri1 = new Triangle(2, 3, 4, 5, 6, 7);
-    tri2 = new Triangle(2, 3, point1, point2);
-    tri3 = new Triangle(5.5, 6.6, 3.3, 4.4, 7.7, 8.8);
-
-
+    tri2 = new Triangle(point1, point2, point3);
+    tri3 = new Triangle(1, 2, 2, 1, 3, 3);
+    
   }
 
   /**
@@ -51,10 +51,10 @@ public class ShapeTest {
         "Triangle: Reference point:(2.000,3.000) Second point:(4.000,5.000) Third point:(6.000,7.000)",
         tri1.toString());
     assertEquals(
-        "Triangle: Reference point:(2.000,3.000) Second point:(4.000,5.000) Third point:(6.000,7.000)",
+        "Triangle: Reference point:(0.000,0.000) Second point:(3.000,0.000) Third point:(0.000,4.000)",
         tri2.toString());
     assertEquals(
-        "Triangle: Reference point:(5.500,6.600) Second point:(3.300,4.400) Third point:(7.700,8.800)",
+        "Triangle: Reference point:(1.000,2.000) Second point:(2.000,1.000) Third point:(3.000,3.000)",
         tri3.toString());
   }
 
@@ -68,9 +68,9 @@ public class ShapeTest {
     assertEquals(Math.PI * 400, circle3.area(), 0.001);
     assertEquals(5, rect1.area(), 0.001);
     assertEquals(100, rect2.area(), 0.001);
-    assertEquals(0.0, tri1.area(), 0.001);
-    assertEquals(40,tri2.area(), 0.001);
-    assertEquals(9, tri3.area(), 0.001);
+    assertEquals(0.0, tri1.area(), 0.001);//colinear triangle
+    assertEquals(6.0,tri2.area(), 0.001);
+    assertEquals(1.5, tri3.area(), 0.001);
   }
 
   /**
@@ -83,31 +83,34 @@ public class ShapeTest {
     assertEquals(2 * Math.PI * 20, circle3.perimeter(), 0.001);
     assertEquals(9, rect1.perimeter(), 0.001);
     assertEquals(40, rect2.perimeter(), 0.001);
-    assertEquals(9, tri1.perimeter(), 0.001);
-    assertEquals(40,tri2.perimeter(), 0.001);
-    assertEquals(9, tri3.perimeter(), 0.001);
+    assertEquals(11.313, tri1.perimeter(), 0.001);
+    assertEquals(12.0,tri2.perimeter(), 0.001);
+    assertEquals(5.886, tri3.perimeter(), 0.001);
 
   }
 
   @Test
   public void testResizes() {
     Shape resizedCircle1, resizedCircle2, resizedCircle3, resizedRect1,
-        resizedRect2;
+        resizedRect2, resizedTri1, resizedTri2, resizedTri3;
 
     resizedCircle1 = circle1.resize(2.5);
     resizedCircle2 = circle2.resize(0);
     resizedCircle3 = circle3.resize(10);
     resizedRect1 = rect1.resize(12.5);
     resizedRect2 = rect2.resize(0.001);
+    resizedTri1 = tri1.resize(0.5);
+    resizedTri2 = tri2.resize(2);
+    resizedTri3 = tri3.resize(5);
 
     assertEquals(2.5 * circle1.area(), resizedCircle1.area(), 0.001);
     assertEquals(0 * circle2.area(), resizedCircle2.area(), 0.001);
     assertEquals(10 * circle3.area(), resizedCircle3.area(), 0.001);
     assertEquals(12.5 * rect1.area(), resizedRect1.area(), 0.001);
     assertEquals(0.001 * rect2.area(), resizedRect2.area(), 0.001);
-  }
-  @Test
-  public void testcompareTo(Shape s){
+    assertEquals(0.5 * tri1.area(), resizedTri1.area(), 0.001);
+    assertEquals(2 * tri2.area(), resizedTri2.area(), 0.001);
+    assertEquals(5 * tri3.area(), resizedTri3.area(), 0.001);
 
   }
 
